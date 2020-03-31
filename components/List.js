7a15282ec24783df56144efe80a3c1c77bcff04a
rafaelmonroy <rafaelmonroy1990@gmail.com>
2020-03-31 16:18:09 -0700

    map running, marker added, firebase added, nav menu added, 3 screens added, currently testing firebase connection

--- components/List.js
+++ components/List.js
@@ -1,11 +1,28 @@
 import React, {Component} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 export default class List extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'LIST VIEW',
+    };
+  }
+
+  componentDidMount() {
+    firestore()
+      .collection('bars')
+      .onSnapshot(querySnapshot => {
+        console.log('success');
+      });
+    console.log('test');
+  }
+
   render() {
     return (
       <View style={styles.main}>
-        <Text style={styles.text}>List Screen</Text>
+        <Text style={styles.text}>{this.state.text}</Text>
       </View>
     );
   }
